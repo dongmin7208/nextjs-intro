1a150b0aaff236641331df362750d0de00554b38
dongmin7208 <sayg3o3d@gmail.com>
2022-08-03 23:00:40 +0900

    ADD: NavCss

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "components/NavBar.module.css";
 export default function NavBar() {
     const router = useRouter();
     return (
-        <nav>
+        <nav className={styles.nav}>
             <Link href="/">
                 <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>
                     Home
